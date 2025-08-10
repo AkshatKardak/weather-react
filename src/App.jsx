0e9c5d13f7b8c35d3eb8140c31ee324071d22fb8
AkshatKardak Github <kardakakshat@gmail.com>
2025-08-10 13:08:36 +0530

    Main Folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
+import Dashboard from "./Components/Dashboard/Dashboard";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Dashboard />
+      
+    </>
+  );
+};
+
+export default App;
